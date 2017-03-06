0ea5b5f3b6d469d960f1f31905243607fe2931e0
Angie Condor <angiegita1602@gmail.com>
2017-03-06 16:30:53 -0500

    first commit maqueta

---
+++ assets/js/app.js
@@ -0,0 +1,28 @@
+  var nombreCoder = prompt("¿Cuál es tu nombre Coder?");
+
+function myFunction() {
+	document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+	if (!event.target.matches('.dropbtn')) {
+
+		var dropdowns = document.getElementsByClassName("dropdown-content");
+		var i;
+		for (i = 0; i < dropdowns.length; i++) {
+			var openDropdown = dropdowns[i];
+			if (openDropdown.classList.contains('show')) {
+				openDropdown.classList.remove('show');
+			}
+		}
+	}
+}
+
+var mostarOcultar = function(divtexte) {
+	var divtext = document.getElementById(divtexte);
+	if (divtext.style.display == "block" ) {
+		divtext.style.display = "none";
+	} else{
+		divtext.style.display = "block";
+	}
+}
